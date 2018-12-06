5b899953203bce63c948217d21183c68a3b12494
julien lopez <julien.lopez51@gmail.com>
2018-12-06 10:52:42 +0100

    moving back to catch header only file

--- unit_tests/main.cpp
+++ unit_tests/main.cpp
@@ -1,17 +1,2 @@
-#include <catch2/internal/catch_interfaces_reporter.h>
-#include <catch2/internal/catch_reporter_registrars.hpp>
-#include <catch2/internal/catch_session.h>
-#include <catch2/reporters/catch_reporter_console.h>
-#include <catch2/reporters/catch_reporter_junit.h>
-
-namespace Catch
-{
-
-CATCH_REGISTER_REPORTER("console", ConsoleReporter)
-CATCH_REGISTER_REPORTER("junit", JunitReporter)
-}
-
-int main(int argc, char* argv[])
-{
-    return Catch::Session().run(argc, argv);
-}
+#define CATCH_CONFIG_MAIN
+#include "catch2/catch.hpp"
